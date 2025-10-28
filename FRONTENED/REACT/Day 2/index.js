6e538a54a233a21a0afb05f8fb433836e9104309
Chandra Prakash Rai <crai60433@gmail.com>
2025-10-28 18:53:42 +0530

    About CDN and diffrent react features

---
+++ FRONTENED/REACT/Day 2/index.js
@@ -0,0 +1,7 @@
+const element = React.createElement("h1" , {} , "Hello EveryOne...");
+
+// ReactDOM.render(element , document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(element);
