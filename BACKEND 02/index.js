2cae4b9c8e747c95944cdc187b0dcf1788d5d97c
ChandraPrakashRai <crai60433@gmail.com>
2025-12-02 00:14:27 +0530

    BHAI bahut easy hai Bahut

---
+++ BACKEND 02/index.js
@@ -0,0 +1,20 @@
+console.log("Hello");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/cp' , (req , res)=> {
+    res.send('CP Kaisa hai')
+})
+app.get('/youtube', (req, res) => {
+    res.send('<a href="https://www.youtube.com/" target="_blank">Open YouTube</a>');
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
